20546ccaabb372c7c78546c5b9756f9bf907e9f7
Pavel Klimovich <115665630+klimovich80@users.noreply.github.com>
2022-12-07 11:57:31 +0300

    draft done

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,46 @@
-
-let popup = document.querySelector('.popup');
-let form=popup.querySelector('.popup__container');
-let input = form.querySelectorAll('.popup__input');
-let formName=input[0];
-let formCredentials = input[1];
-let formSubmit = form.querySelector('.popup__button');
-let closeIcon = document.querySelector('.popup__close-icon');
-let profileEditButton = document.querySelector('.profile__edit-button'); 
-let profileTitle = document.querySelector('.profile__title');
-let profileSubitle = document.querySelector('.profile__subtitle');
-
+//блок переменных
+let popup = document.querySelector('.popup');                            //попап окно
+let form=popup.querySelector('.popup__container');                       //попап форма
+let input = form.querySelectorAll('.popup__input');                      //инпуты формы
+let formName=input[0];                                                   //поле редактирования имени в форме
+let formCredentials = input[1];                                          //поле редактирования описания в форме
+let formSubmit = form.querySelector('.popup__button');                   //кнопка сохранения формы
+let closeIcon = document.querySelector('.popup__close-icon');            //иконка закрытия формы
+let profileEditButton = document.querySelector('.profile__edit-button'); //кнопка редактирования профиля
+let profileTitle = document.querySelector('.profile__title');            //имя профиля
+let profileSubitle = document.querySelector('.profile__subtitle');       //описание профиля
+//скрываем попап
 function hidePopup(item){
+  //убираем класс отображения из элемента
     item.classList.remove('popup__opened');
   }
-
+//показываем попап
 function showPopup(item) {
+  //добавляем класс отображения в элемент
   item.classList.add('popup__opened');
+  //копируем значения имени и описания из профиля в форму
   formName.setAttribute('value', profileTitle.innerText);
   formCredentials.setAttribute('value', profileSubitle.innerText);
 }
-
+// клик по иконке
 closeIcon.onclick=function() {
+  //прячем форму
   hidePopup(popup);
 };
-
+//клик на кнопке редактирования
 profileEditButton.onclick = function () {
+  //показываем форму
   showPopup(popup);
 };
-
+//сохраняем заполненную форму
 formSubmit.onclick = function(e){
+  //сбрасываем действие кнопки по умолчнию
   e.preventDefault();
+  //копируем значения полей из формы в профиль
   profileTitle.innerText = formName.value;
   profileSubitle.innerText = formCredentials.value;
+  //прячем форму
   hidePopup(popup);
-  console.log('form submitted');
 };
 
 
