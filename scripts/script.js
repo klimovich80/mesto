0f2dbc77b8571b83945ffefb16cc8eada93c3988
Pavel Klimovich <115665630+klimovich80@users.noreply.github.com>
2022-12-07 11:00:38 +0300

    popup done

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 
 let popup = document.querySelector('.popup');
-let form=popup.querySelector('.popup__form');
-// let input = form.querySelectorAll('.popup__input');
-let popupOpened = document.querySelector('.popup__opened'); 
+let form=popup.querySelector('.popup__container');
+let input = form.querySelectorAll('.popup__input');
+let formName=input[0];
+let formCredentials = input[1];
+let formSubmit = form.querySelector('.popup__button');
 let closeIcon = document.querySelector('.popup__close-icon');
 let profileEditButton = document.querySelector('.profile__edit-button'); 
+let profileTitle = document.querySelector('.profile__title');
+let profileSubitle = document.querySelector('.profile__subtitle');
 
 function hidePopup(item){
     item.classList.remove('popup__opened');
@@ -12,6 +16,8 @@ function hidePopup(item){
 
 function showPopup(item) {
   item.classList.add('popup__opened');
+  formName.setAttribute('value', profileTitle.innerText);
+  formCredentials.setAttribute('value', profileSubitle.innerText);
 }
 
 closeIcon.onclick=function() {
@@ -20,6 +26,14 @@ closeIcon.onclick=function() {
 
 profileEditButton.onclick = function () {
   showPopup(popup);
+};
+
+formSubmit.onclick = function(e){
+  e.preventDefault();
+  profileTitle.innerText = formName.value;
+  profileSubitle.innerText = formCredentials.value;
+  hidePopup(popup);
+  console.log('form submitted');
 };
 
 
