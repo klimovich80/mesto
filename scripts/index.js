aab47657f6286a1fada042233ed00c16729f9a20
Pavel Klimovich <115665630+klimovich80@users.noreply.github.com>
2022-12-09 08:13:08 +0300

    fix: popup PP corrections

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,38 @@
 //блок переменных
-const popup = document.querySelector('.popup');                            //попап окно
-const form=popup.querySelector('.popup__form');                              //попап форма
-const formName = form.querySelector('.popup__input_type_name');              //поле редактирования имени в форме
-const formCredentials = form.querySelector('.popup__input_type_credentials');//поле редактирования описания в форме
-const closeIcon = document.querySelector('.popup__close-icon');              //иконка закрытия формы
-const profileEditButton = document.querySelector('.profile__edit-button');   //кнопка редактирования профиля
-const profileName = document.querySelector('.profile__title');              //имя профиля
-const profileCredentials = document.querySelector('.profile__subtitle');         //описание профиля
+const popup = document.querySelector(".popup"); //попап окно
+const form = popup.querySelector(".popup__form"); //попап форма
+const formName = form.querySelector(".popup__input_type_name"); //поле редактирования имени в форме
+const formCredentials = form.querySelector(".popup__input_type_credentials"); //поле редактирования описания в форме
+const closeIcon = document.querySelector(".popup__close-icon"); //иконка закрытия формы
+const profileEditButton = document.querySelector(".profile__edit-button"); //кнопка редактирования профиля
+const profileName = document.querySelector(".profile__title"); //имя профиля
+const profileCredentials = document.querySelector(".profile__subtitle"); //описание профиля
 
 //скрываем попап
-function hidePopup(){
+function hidePopup() {
   //убираем класс отображения из элемента
-  popup.classList.remove('popup_opened');
+  popup.classList.remove("popup_opened");
 }
 //показываем попап
 function showPopup() {
   //добавляем класс отображения в элемент
-  popup.classList.add('popup_opened');
+  popup.classList.add("popup_opened");
   //копируем значения имени и описания из профиля в форму
   formName.value = profileName.textContent;
   formCredentials.value = profileCredentials.textContent;
 }
 
-function handleFormSubmit (e){
+function handleFormSubmit(e) {
   e.preventDefault();
   //копируем значения полей из формы в профиль
   profileName.textContent = formName.value;
   profileCredentials.textContent = formCredentials.value;
   //прячем форму
-  hidePopup(popup);
+  hidePopup();
 }
 
-closeIcon.addEventListener('click', hidePopup);
+closeIcon.addEventListener("click", hidePopup);
 //клик на кнопке редактирования
-profileEditButton.addEventListener('click', showPopup);
+profileEditButton.addEventListener("click", showPopup);
 //сохраняем и закрываем заполненную форму при нажатии кнопки
-form.addEventListener('submit', handleFormSubmit);
-
-
+form.addEventListener("submit", handleFormSubmit);
