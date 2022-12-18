853f109a3b536db365c8253ee5ea27140fe857a1
Pavel Klimovich <115665630+klimovich80@users.noreply.github.com>
2022-12-18 13:56:23 +0300

    feat: popup js template done

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,14 @@
 //блок переменных
 const root = document.querySelector(".root");
-//const trashButton = root.querySelector(".element__trash"); //кнопка удаления карточки
-//const likeButton = root.querySelector(".element__like");
-const templateElement = root.querySelector("#tempElement").content; //содержиое шаблона
+const main= root.querySelector(".main");
+const templateElement = root.querySelector("#tempElement").content; //шаблон картинки
+const templatePopup = root.querySelector("#templatePopup").content; //шаблон вслывающего окна
 const elements = root.querySelector(".elements__items");
 const popup = root.querySelector(".popup"); //попап окно
-const form = popup.querySelector(".popup__form"); //попап форма
-const formName = form.querySelector(".popup__input_type_name"); //поле редактирования имени в форме
-const formCredentials = form.querySelector(".popup__input_type_credentials"); //поле редактирования описания в форме
+//const form = popup.querySelector(".popup__form"); //попап форма
+//const formName = form.querySelector(".popup__input_type_name"); //поле редактирования имени в форме
+//const formCredentials = form.querySelector(".popup__input_type_credentials"); //поле редактирования описания в форме
 const closeIcon = root.querySelector(".popup__close-icon"); //иконка закрытия формы
-const profileEditButton = root.querySelector(".profile__edit-button"); //кнопка редактирования профиля
 const profileName = root.querySelector(".profile__title"); //имя профиля
 const profileCredentials = root.querySelector(".profile__subtitle"); //описание профиля
 const initialCards = [
@@ -51,12 +50,21 @@ function hidePopup() {
   popup.classList.remove("popup_opened");
 }
 //показываем попап
-function showPopup() {
-  //добавляем класс отображения в элемент
-  popup.classList.add("popup_opened");
-  //копируем значения имени и описания из профиля в форму
-  formName.value = profileName.textContent;
-  formCredentials.value = profileCredentials.textContent;
+function showPopup(elem,title,name,value,button_text) {
+  // //добавляем класс отображения в элемент
+  // popup.classList.add("popup_opened");
+  // //копируем значения имени и описания из профиля в форму
+  // formName.value = profileName.textContent;
+  // formCredentials.value = profileCredentials.textContent;
+  alert('showing popup');
+  const pUp=templatePopup.cloneNode(true);
+  pUp.querySelector('.popup__title').textContent=title;
+  pUp.querySelector('.popup__input_type_name').setAttribute('placeholder',name);//placeholder=name;
+  pUp.querySelector('.popup__input_type_credentials').setAttribute('placeholder',value);
+  pUp.querySelector('.popup__button').setAttribute('value',button_text);
+  pUp.querySelector('.popup__button').textContent=button_text;
+  pUp.querySelector('.popup').classList.add("popup_opened");
+  main.append(pUp);
 }
 
 function handleFormSubmit(e) {
@@ -69,8 +77,8 @@ function handleFormSubmit(e) {
 }
 
 function diplayCard(source) {
-  trashButton = root.querySelector(".element__trash"); //кнопка удаления карточки
-  likeButton = root.querySelector(".element__like");
+  //const trashButton = root.querySelector(".element__trash"); //кнопка удаления карточки
+  //const likeButton = root.querySelector(".element__like");
   source.forEach((item) => {
     // клонируем содержимое тега template
     const card = templateElement.cloneNode(true);
@@ -90,15 +98,16 @@ function likeCard(elem) {
   elem.classList.toggle("element__like_checked");
 }
 
-closeIcon.addEventListener("click", hidePopup);
+//closeIcon.addEventListener("click", hidePopup);
 //клик на кнопке редактирования
-profileEditButton.addEventListener("click", showPopup);
+//profileEditButton.addEventListener("click", showPopup);
 //сохраняем и закрываем заполненную форму при нажатии кнопки
-form.addEventListener("submit", handleFormSubmit);
+//form.addEventListener("submit", handleFormSubmit);
 //показ карточек на загрузке страницы
 document.addEventListener("DOMContentLoaded", () => {
   diplayCard(initialCards);
 });
+
 root.addEventListener("click", (e) => {
   // switch case?
   if (e.target.classList.contains("element__like")) {
@@ -107,7 +116,11 @@ root.addEventListener("click", (e) => {
     deleteCard(e.target);
   } else if (e.target.classList.contains("profile__add-button")) {
     alert("add card");
+    showPopup(e.target);
   } else if (e.target.classList.contains("profile__edit-button")) {
     alert("edit profile");
+    showPopup(e.target,'Редактировать профиль','Введите имя','Немного о себе','Сохранить');
+  }else{
+    alert(e.target);
   }
 });
