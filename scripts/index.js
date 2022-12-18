ff8962c8a4c1e2ddb657e8fa13c8a468fc5fe7fc
Pavel Klimovich <115665630+klimovich80@users.noreply.github.com>
2022-12-18 11:27:14 +0300

    feat: cards js done

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,48 @@
 //блок переменных
-const popup = document.querySelector(".popup"); //попап окно
+const root = document.querySelector(".root");
+const trashButton = root.querySelector(".element__trash"); //кнопка удаления карточки
+const templateElement = root.querySelector("#tempElement").content; //содержиое шаблона
+const elements=root.querySelector('.elements__items');
+const popup = root.querySelector(".popup"); //попап окно
 const form = popup.querySelector(".popup__form"); //попап форма
 const formName = form.querySelector(".popup__input_type_name"); //поле редактирования имени в форме
 const formCredentials = form.querySelector(".popup__input_type_credentials"); //поле редактирования описания в форме
-const closeIcon = document.querySelector(".popup__close-icon"); //иконка закрытия формы
-const profileEditButton = document.querySelector(".profile__edit-button"); //кнопка редактирования профиля
-const profileName = document.querySelector(".profile__title"); //имя профиля
-const profileCredentials = document.querySelector(".profile__subtitle"); //описание профиля
+const closeIcon = root.querySelector(".popup__close-icon"); //иконка закрытия формы
+const profileEditButton = root.querySelector(".profile__edit-button"); //кнопка редактирования профиля
+const profileName = root.querySelector(".profile__title"); //имя профиля
+const profileCredentials = root.querySelector(".profile__subtitle"); //описание профиля
+const initialCards = [
+  {
+    name: "Архыз",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
 
 //скрываем попап
 function hidePopup() {
@@ -31,8 +67,23 @@ function handleFormSubmit(e) {
   hidePopup();
 }
 
+function diplayCard(source) {
+  source.forEach(item => {
+    // клонируем содержимое тега template
+  const card = templateElement.cloneNode(true);
+  // наполняем содержимым
+  card.querySelector('.element__image').src=item.link;
+  card.querySelector('.element__caption').textContent=item.name;
+  // отображаем на странице
+  elements.prepend(card);
+  });
+  
+}
+
 closeIcon.addEventListener("click", hidePopup);
 //клик на кнопке редактирования
 profileEditButton.addEventListener("click", showPopup);
 //сохраняем и закрываем заполненную форму при нажатии кнопки
 form.addEventListener("submit", handleFormSubmit);
+//показ карточек на загрузке страницы
+document.addEventListener("DOMContentLoaded", ()=>{diplayCard(initialCards);});
