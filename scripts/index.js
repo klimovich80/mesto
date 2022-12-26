644a4fd5c43fd0601e47f6eaceedc44d1ea0cdd3
Pavel Klimovich <115665630+klimovich80@users.noreply.github.com>
2022-12-26 16:49:26 +0300

    fix: adding card done

--- scripts/index.js
+++ scripts/index.js
@@ -36,46 +36,6 @@ function submitForm(element) {
   element.target.reset();
   closePopup(element.target.closest(".popup"));
 }
-//создание карточек
-// function createCard(source) {
-//   source.forEach((item) => {
-//     const card = templateCard.cloneNode(true);
-//     card.querySelector(".element__image").src = item.link;
-//     card.querySelector(".element__image").alt = item.name;
-//     card.querySelector(".element__caption").textContent = item.name;
-//     card
-//       .querySelector(".element__trash")
-//       .addEventListener("click", (element) =>
-//         deleteCard(element.target.closest(".element"))
-//       );
-//     card
-//       .querySelector(".element__like")
-//       .addEventListener("click", (element) => likeCard(element.target));
-//     card
-//       .querySelector(".element__image")
-//       .addEventListener("click", (element) => openPhotoPopup(element)); //
-//     renderCard(card);
-//   });
-// }
-// //отрисовка карточки
-// function renderCard(card) {
-//   elements.prepend(card);
-// }
-// //удаление карточки
-// function deleteCard(card) {
-//   card.remove();
-// }
-// //лайк
-// function likeCard(card) {
-//   card.classList.toggle("element__like_checked");
-// }
-//--создание попапов--
-function openPhotoPopup(element) {
-  imageSource.src = element.target.src;
-  imageSource.alt = element.target.alt;
-  imageCaption.textContent = element.target.alt;
-  openPopup(imagePopup);
-}
 function createAddPopup() {
   openPopup(addCardPopup);
 }
@@ -86,14 +46,10 @@ function createEditPopup() {
 }
 //зполнение форм
 function submitAddForm(event) {
-  console.log("event: ", event);
-  const card = {
-    name: "test",
-    link:
-      "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/30b97543333915.57eb79961a3de.png",
-  };
-  card.name = addCardPlace.value;
-  card.link = addCardUrl.value;
+  nameValue = addCardPlace.value;
+  urlValue = addCardUrl.value;
+  const newCard=getCard({name: nameValue, link: urlValue});
+  elements.prepend(newCard);
   submitForm(event);
 }
 function submitEditForm(event) {
@@ -158,7 +114,6 @@ function getCard(item) {
   function likeCard(card) {
     card.classList.toggle("element__like_checked");
   }
-
   return cardTemplate;
 }
 
