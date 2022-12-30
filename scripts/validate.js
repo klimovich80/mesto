111382aab4bf8adae40df8310fcbdb73ce12e37a
Pavel Klimovich <115665630+klimovich80@users.noreply.github.com>
2022-12-30 10:49:18 +0300

    dev: validation requirements

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,13 @@
 enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-}); 
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+});
+//TODO !Требования к валидации форм. 
+//Разбейте код валидации на функции. 
+//Вы уже делали это в теме «Валидация форм». 
+//Сделайте функцию enableValidation ответственной за включение валидации всех форм. 
+//Пусть она принимает как объект настроек все нужные функциям классы и селекторы элементов
