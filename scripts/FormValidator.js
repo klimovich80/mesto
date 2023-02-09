fb054ca53d03ab8070584acf7d9fee82e77048cf
Pavel Klimovich <115665630+klimovich80@users.noreply.github.com>
2023-02-09 18:53:01 +0300

    feat: FormValidator for each form

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -7,4 +7,42 @@ class FormValidator {
     this._inputErrorClass = validationConfig.inputErrorClass;
     this._errorClass = validationConfig.errorClass;
   }
+
+  enableValidation() {
+    this._setEventListeners();
+  }
+
+  _setEventListeners() {
+    const _formArray = Array.from(
+      document.querySelectorAll(this._formSelector)
+    );
+    _formArray.forEach((form) => {
+      const _inputArray = Array.from(
+        form.querySelectorAll(this._inputSelector)
+      );
+      //проверить нечальное состояние кнопки
+      this._toggleButtonState(
+        form,
+        _inputArray,
+        this._submitButtonSelector,
+        this._inactiveButtonClass
+      );
+
+      _inputArray.forEach((inputElement) => {
+        inputElement.addEventListener("input", () => {
+          this._checkInputValidity(form, inputElement);
+          this._toggleButtonState(
+            form,
+            _inputArray,
+            this._submitButtonSelector,
+            this._inactiveButtonClass
+          );
+        });
+      });
+    });
+  }
+
+  _toggleButtonState() {}
+  _checkInputValidity() {}
 }
+
