7be60a85d0d58572e086f20f8f243db9aa61659f
Pavel Klimovich <115665630+klimovich80@users.noreply.github.com>
2023-02-25 09:38:33 +0300

    feat: webpack new config

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
       },
       {
         test: /\.css$/,
@@ -52,4 +55,5 @@ module.exports = {
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
   ],
+  devtool: "eval-source-map",
 };
